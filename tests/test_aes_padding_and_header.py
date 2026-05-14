8b8da3569310bdd38ecc9ffbd6031cc8572bb3c9
nguyenbach23 <lockedbach1@gmail.com>
2026-05-14 21:03:19 +0700

    Update test_aes_padding_and_header.py

--- tests/test_aes_padding_and_header.py
+++ tests/test_aes_padding_and_header.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from aes_socket_utils import (
     build_data_packet,
     build_key_packet,
